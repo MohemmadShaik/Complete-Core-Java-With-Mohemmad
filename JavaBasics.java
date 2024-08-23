81530177ac633e253b58c33e62de163bdf446b47
MohemmadShaik <mohemmadshaik@gmail.com>
2024-08-23 21:19:28 +0530

    Added Pre-Requisites and one Java file - JavaBasics which covers Variables and Datatypes

---
+++ JavaBasics.java
@@ -0,0 +1,92 @@
+/* 6. Variables and Datatypes
+
+6.1 Creating Java File
+6.2 Boilerplate Code
+6.3 Output in Java
+6.4 Print a Pattern
+6.5 Variables in Java	
+6.6 Data Types in Java
+6.7 Sum of a b
+6.8 Comments in Java
+6.9 Input in Java
+6.10 Sum of a b ( Input from User )
+6.11 Product of a b
+6.12 Area of a Circle
+6.13 Type Conversion
+6.14 Type Casting
+6.15 Type Promotion in Expression
+6.16 How does Java code run
+6.17 Variable Questions FAQ's */
+
+
+/*  6.1 Creating a Simple Java File program by saying Hello to the whole World through java Program 
+
+
+
+*/
+public class JavaBasics {
+
+    public static void main(String[] args) {
+      
+    // System.out.println("Hello/nWorld\nHellooo \n World");
+
+     /* Permeter of Rectangle :
+     
+         int a=10;
+         int b=5;
+    //   int p= 2 * (a+b);
+    //   System.out.println("Perimiter of Rectangle " +a+ " and "+b+"  is : "+p);
+    */
+
+    // Sum of two numbers 
+    int a1=10;
+    int b1=5;
+    int sum = a1 + b1;
+    System.out.println(sum);
+
+    // Primitives Data Types
+
+    byte b2 = 8;
+    System.out.println(b2);
+    char ch = 'a';
+    System.out.println(ch);
+    boolean var = true;
+    System.out.println(var);
+    float price = 10.6f; // or float price = (float) 10.5; // No error, explicit casting from double to float
+    System.out.println(price);
+    int number = 25;
+    System.out.println("int number :  "+number);
+    /* Primitive Data types and their sizes
+     byte : 1 byte
+     boolean : 1 byte
+     char : 2 bytes
+     short: 2 bytes
+     int : 4 bytes
+     float : 4 bytes
+     long : 8 bytes
+     double : 8 bytes */
+
+
+
+/*  6.9 Input in Java
+
+next
+nextLine
+nextByte
+nextBoolean
+nextInt
+nextShort
+nextLong
+nextFloat
+nextDouble */
+
+
+     
+     
+
+
+     
+    
+    
+    }
+}
--- Readme.md
+++ Readme.md
@@ -1,1 +1,20 @@
+Complete Core Java Program from Scratch : 
+
+Learn Complete Java
+Learn Complete Data Structures & Algorithms
+
+The curious part in our programming journey is we will cover all the topics with Practice Questions.
+
+Pre-Requisites
+==============
+
+A Laptop + Internet
+Install java + Code Editor 
+Note : Here we were using VS-Code editor 
+Notebook + Pen
+
+Author : Mohemmad
+Reference : Apna College 
+gmail : mohemmadshaik@gmail.com
+Username : MohemmadShaik
 
