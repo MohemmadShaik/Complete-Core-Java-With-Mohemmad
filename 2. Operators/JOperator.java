717f588bd577345992354f856bdfa8bb849e75c2
MohemmadShaik <mohemmadshaik@gmail.com>
2024-08-26 23:17:36 +0530

    7 .1 to 7.6 topics covered successfully

--- 2. Operators/JOperator.java
+++ 2. Operators/JOperator.java
@@ -1,18 +1,20 @@
 
-// 7.1 Arithmetic Operators ---> Binary
+// 7.2 Arithmetic Operators ---> Binary
 public class JOperator {
 
     public static void main(String[] args) {
-        int A = 10;
+/*      int A = 10;
         int B = 5;
         System.out.println("Addition of "+A+" + "+B+" is "+(A+B)+"\n");
         System.out.println("Subtract of "+A+" - "+B+" is "+(A-B)+"\n");
         System.out.println("Multiply of "+A+" X "+B+" is "+(A*B)+"\n");
         System.out.println("Divide of "+A+" / "+B+" is "+(A/B)+"\n");
         System.out.println("Modulo  of "+A+" % "+B+" is "+(A%B)+"\n");
-
+         
         System.out.println("========= >> UNARY OPerator << ==============");
-    // 7.1 Arithmetic Operator ---> Unary
+   */
+
+  /*  // 7.3 Arithmetic Operator ---> Unary
 
     //Post increment
     int a = 40;
@@ -49,7 +51,88 @@ public class JOperator {
     System.out.println("Value of b :"+f1);//65
 
     System.out.println("============>End of Post decrement<===========");
+*/
+ 
+/* // 7.4 Relational Operators ( == , != , > , < , >= , <=) 
 
+    int a= 10;
+    int b =30;
+    System.out.println(a == b); //false
+    System.out.println(a!=b); //true
+    System.out.println(a>b); //false
+    System.out.println(a<b); //true
+    System.out.println(a>=b);//false
+    System.out.println(a<=b);//true
+    
+    b=10;
+    System.out.println( a==b); //true
+    System.out.println(a!=b); //false
+    System.out.println(a>b); //false
+    System.out.println(a<b); //false
+    System.out.println(a>=b);//true
+    System.out.println(a<=b);//true
+
+*/
+
+ /*    //7.5 Logical Operators ( && (Logical AND), || (Logical OR), ! (Logical NOT))
+
+    //Logical AND (&&) : 
+
+    System.out.println("=========> Logical AND (&&) <========");
+
+    System.out.println((3>2) && (5>0)) ; //True
+    System.out.println((3>2) && (5<0) ); //False
+    System.out.println((3<2) && (5>0) ); //False
+    System.out.println((3<2) && (5<0) ); //False
+
+    System.out.println("=========> Logical OR (||) <========");
+    
+    // Logical OR (||) :
+    System.out.println((3>2) || (5>0)) ; //True
+    System.out.println((3>2) || (5<0) ); //True
+    System.out.println((3<2) || (5>0) ); //True
+    System.out.println((3<2) || (5<0) ); //False
+
+    System.out.println("=========> Logical NOT (!) <========");
+
+    // Logical NOT (!) :
+    System.out.println( !(3>2) ) ; //False
+    System.out.println( !(5<0)) ; //True
+
+*/
+    //7.6 Assignment Operators  ( =, += , -=  , *= , /=)
+
+    int A = 10;
+    A += 10; /* A = A + 10; */
+    System.out.println(A);//20
+
+    int B = 8;
+    B = B - 8; /* B-=8; */
+    System.out.println(B);//0
+
+    int C = 30;
+    C *= 30; /* C = C * 30;  */
+    System.out.println(C);//900
+
+    int D = 40;
+    D /= 40; /* D = D / 40 */
+    System.out.println(D);//1
+
+    int E = 40;
+    E %= 40; /* E = E % 40 */
+    System.out.println(E); //0
+
+
+   
+
+
+
+
+
+
+
+    
+    
 
 
     }
