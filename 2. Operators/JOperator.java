830120164c5346eb0236ec9e18053c8ecde9bf91
MohemmadShaik <mohemmadshaik@gmail.com>
2024-08-26 17:28:58 +0530

    7.1 Types of operators --> Arithmetic Operator (Binary) Completed successfully

---
+++ 2. Operators/JOperator.java
@@ -0,0 +1,18 @@
+
+// 7.1 Arithmetic Operators ---> Binary
+public class JOperator {
+
+    public static void main(String[] args) {
+        int A = 10;
+        int B = 5;
+        System.out.println("Addition of "+A+" + "+B+" is "+(A+B)+"\n");
+        System.out.println("Subtract of "+A+" - "+B+" is "+(A-B)+"\n");
+        System.out.println("Multiply of "+A+" X "+B+" is "+(A*B)+"\n");
+        System.out.println("Divide of "+A+" / "+B+" is "+(A/B)+"\n");
+        System.out.println("Modulo  of "+A+" % "+B+" is "+(A%B)+"\n");
+
+    }
+}
+
+// 7.1 Arithmetic Operator ---> Unary
+
