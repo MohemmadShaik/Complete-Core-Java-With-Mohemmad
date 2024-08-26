01e4a9d377167f700711ad43297f47124fa3a30b
MohemmadShaik <mohemmadshaik@gmail.com>
2024-08-26 14:10:15 +0530

    PracticeQuestion4 completed successfully in Variables and Datatypes

---
+++ 1. Variables and Datatypes/PracticeQuestion4.java
@@ -0,0 +1,26 @@
+/* Q4 : what will be the type of result in the following Java code?
+ * 
+ * byte b = 4;
+ * char c = 'a';
+ * short s = 512;
+ * int i = 1000;
+ * float f = 3.14f;
+ * double d = 99.9954;
+ * result = (f * b) + (i % c) - (d * s);   // Here result is a double datatype we confirmed from below program.
+ * 
+  */
+public class PracticeQuestion4 {
+    public static void main(String[] args) {
+        byte b = 4;
+        char c = 'a';
+        short s = 512;
+        int i = 1000;
+        float f = 3.14f;
+        double d = 99.9954;
+        //float result = (f * b) + (i % c) - (d * s);// it clearly says its an Error because the  double data type can't be converted to float type.
+       double result = (f * b) + (i % c) - (d * s); // So the type of the result is : double data type
+       System.out.println("Result is : "+result);
+
+        
+    }
+}
