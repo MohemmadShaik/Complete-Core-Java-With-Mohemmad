db8f5132adeffa2d4efe52348d7ffb1bf37b26f5
MohemmadShaik <mohemmadshaik@gmail.com>
2024-08-26 23:34:12 +0530

    7.8 Q1 Guess the output for the given code completed successfully

---
+++ 2. Operators/JOperatorPracQuest.java
@@ -0,0 +1,14 @@
+// Q1 : Guess the output for the below program
+  public class JOperatorPracQuest {
+    public static void main(String[] args) {
+        int x = 2, y =5;
+        int exp1 = (x * y / x ); //5
+        int exp2 = (x * (y / x) ); //4
+
+        System.out.print(exp1 + " , ");
+        System.out.print(exp2);
+    
+
+        
+    }
+}
