2369abc7970bb04790f86502a5d5c0fc039c58ff
MohemmadShaik <mohemmadshaik@gmail.com>
2024-08-26 23:58:46 +0530

    7.8 Q3 Guess the  output for the given code is completed successfully

---
+++ 2. Operators/JOperatorPQ3.java
@@ -0,0 +1,15 @@
+// Q3 : Guess the output for the below program
+
+public class JOperatorPQ3 {
+    public static void main(String[] args) {
+        int x, y, z;
+        x = y = z = 2;
+        x += y; /* x = x + y // x = 4 */
+        y -= z; /* y = y - z // y =0 */
+        z /= (x + y); /* z = z / (x + y) // z = 0*/
+        System.out.println(x + " " + y + " " + z);
+
+        /* output : 4  0  0 */
+    }
+    
+}
