7c0150e00cf3e19c3958b5a44266c547252388c3
MohemmadShaik <mohemmadshaik@gmail.com>
2024-08-31 04:16:02 +0530

    9.16 Practice questions Q1 and Q2 completed successfully

---
+++ 4. Loops (Flow Control)/JLoopsPQ1.java
@@ -0,0 +1,18 @@
+// 9.16 Loops Questions FAQ's - Practice Questions(PQ)
+/* 
+
+// Q1 : How many times 'Hello' is printed?
+
+public class JLoopsPQ1 {
+    public static void main(String[] args) {
+        for(int i=0; i<5; i++){
+            System.out.print(i+" Before sop ");
+            System.out.println("Hello"); //output : 2 times Hello will be printed
+            i+=2; //i = i+2; 
+            System.out.println(" After sop "+i);
+        }
+    }
+}
+*/
+
+
