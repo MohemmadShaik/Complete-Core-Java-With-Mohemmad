af2a6410f2597138a3f9ff79b05063301cf675df
MohemmadShaik <mohemmadshaik@gmail.com>
2024-08-30 15:17:38 +0530

    9.1 to 9.12 topics covered with Question - break keyword

--- 4. Loops (Flow Control)/JLoops.java
+++ 4. Loops (Flow Control)/JLoops.java
@@ -77,6 +77,122 @@ import java.util.Scanner;
 
  */
 
+ /*  
  //9.6 for Loop
+
+ //Program 1:  to print 10 times Hello World using for loop.
+
+ public class JLoops{
+
+    public static void main(String[] args) {
+        for(int i=1; i<=10 ; i++){
+            System.out.println(i+". Hello World");
+        }
+        System.out.println("Hello World Printed 10x");
+    }
+ }
+*/
+
+
+ //9.7 Print SQUARE Pattern
+/* 
+ public class JLoops{
+    public static void main(String[] args) {
+        for(int i = 1; i <= 4 ; i++){
+            System.out.println("* * * *");
+        }
+    }
+ }
+
+ */
+
+ /* 
+ // 9.8 Print Reverse of a number
+  // $$NOTE1 : To get the last digit of any number use : num%10
+  // $$NOTE2 : To remove the last digit of any number use : num/10
+
+ import java.util.Scanner;;
+  public class JLoops{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number a reverse : ");
+        int num=sc.nextInt();
+        int given_number = num;
+        int reverse = 0;
+        int last_digit = 0;
+        while(num>0){
+            last_digit = num%10;
+            reverse = (reverse * 10) + last_digit;
+            num = num/10;                                                                                                                                                                                                   
+        }
+        System.out.println("given Number : "+given_number+ "\n"+" Reverse Number : "+reverse);
+       sc.close();//close the scanner to avoid resource leaks
+    }
+  }
+
+  */
+
+  
+//9.11 Break Statement : To exit from loop we use break statement
+/* 
+// Program 9.11.1 : If i value is 3 then exit from the loop 
+public class JLoops{
+    public static void main(String[] args) {
+        for (int i = 1; i<=10 ; i++){
+            if(i==3){
+                break; // to exit from this entire for loop
+            }
+            System.out.println(i);
+        }
+        System.out.println("Am out of the loop when i value is 3 ");
+    }
+}
+
+*/
+
+//9.12 Question - break keyword
+ 
+// Program 9.12.1  : Keep entering numbers utill user enters a multiple of 10
+//LOGIC 1 :
+import java.util.Scanner;
+public class JLoops{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        do{
+            
+            System.out.print("Enter a number here : ");
+            int n = sc.nextInt();
+            if(n % 10 == 0){
+                break;
+            }
+            System.out.println(n);
+        }while(true);
+    }
+}
+
+
+/* 
+//LOGIC 2 :
+import java.util.Scanner;
+ public class JLoops{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter any random number not multiply by 10 : ");
+        int num = sc.nextInt();
+        while(num>0){
+            if(num%10==0){
+                break;
+            }
+            System.out.println(num);
+            // Ask for the next number
+            System.out.print("Enter another number: ");
+            num = sc.nextInt();
+        }
+        System.out.println("exit loop since "+num+" is multiply by 10.");
+    }
+ }
+
+*/
+
 
 
