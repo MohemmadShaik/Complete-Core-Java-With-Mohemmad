78a0f89185731b8e65de00c22ebcea09c931c307
MohemmadShaik <mohemmadshaik@gmail.com>
2024-08-31 16:03:13 +0530

    need to update PatternPart_1.java file

--- 5. Patterns Part 1/PatternPart_1.java
+++ 5. Patterns Part 1/PatternPart_1.java
@@ -1,3 +1,7 @@
+//10. Patterns Part 1
+
+/* 
+// Program 1 : To print STAR pattern  (Nested loops)
 public class PatternPart_1 {
 
     public static void main(String[] args) {
@@ -7,6 +11,22 @@ public class PatternPart_1 {
                 System.out.print("*"+" ");
             }
             System.out.println("\n");
+        }
+    }
+}
+
+*/
+
+//Program 2 : print inverted-star pattern
+
+public class PatternPart_1 {
+
+    public static void main(String[] args) {
+        for(int i =1; i<=4; i++){
+            for(int j=1; j<=1; j--){
+                System.out.print("*");
+            }
+            System.out.println();
         }
     }
 }
