5d37ef8bdc9dbd8f18124ef9f3e026d02819d864
MohemmadShaik <mohemmadshaik@gmail.com>
2024-08-31 19:39:54 +0530

    10.1 to 10.4 topics completed successfully

--- 5. Patterns Part 1/PatternPart_1.java
+++ 5. Patterns Part 1/PatternPart_1.java
@@ -1,7 +1,7 @@
 //10. Patterns Part 1
 
 /* 
-// Program 1 : To print STAR pattern  (Nested loops)
+// 10.1 Program 1 : To print STAR pattern  (Nested loops)
 public class PatternPart_1 {
 
     public static void main(String[] args) {
@@ -17,14 +17,63 @@ public class PatternPart_1 {
 
 */
 
-//Program 2 : print inverted-star pattern
+// 10.2Program : print inverted-star pattern
 
+//Logic -1 : 
+/* 
 public class PatternPart_1 {
 
     public static void main(String[] args) {
-        for(int i =1; i<=4; i++){
-            for(int j=1; j<=1; j--){
-                System.out.print("*");
+        for (int i = 1; i <= 4; i++) {
+            for (int j = i; j <= 4; j++) {
+                System.out.print("*" + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+ */
+
+//Logic 2 :
+/* 
+public class PatternPart_1 {
+
+    public static void main(String[] args) {
+        int n=4;
+        for(int i=1; i<=n; i++){
+            for(int j=1 ; j<= (n-i+1) ; j++){
+                System.out.print("*"+" ");
+            }
+            System.out.println();
+        }
+    }
+}
+*/
+
+// 10.3 Program : Print HALF-PYRAMID pattern
+/* 
+public class PatternPart_1 {
+
+    public static void main(String[] args) {
+        for(int i=1; i<=4; i++){
+            for(int j=1; j<=i; j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
+    */
+
+// Program 10.4 : Print CHARACTER pattern
+public class PatternPart_1 {
+    public static void main(String[] args) {
+        int n = 5;
+        char ch = 'A';
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(ch+" ");
+                ch++;
             }
             System.out.println();
         }
