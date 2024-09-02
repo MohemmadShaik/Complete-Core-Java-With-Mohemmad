e96babcf9fb6cf9af44111004eb2942f180b6c60
MohemmadShaik <mohemmadshaik@gmail.com>
2024-09-02 23:20:05 +0530

    Part 2 : feature1 ==> using function without passing any arguments and return type.

--- 6. Funtions and  Methods/MyFuncGitBranches.java
+++ 6. Funtions and  Methods/MyFuncGitBranches.java
@@ -1,16 +1,20 @@
 //Program 2: Addition of 2 numbers with and with out using functions
 
 //part 1 : main branch : without using function (i.e Simple basic sum program ).
+/* Note : 1. we are in main branch
+          2. git commit -m "Part 1 : without using function (i.e Simple basic sum program )." */
+
+//Part 2 : using feature1 branch : using function but without passing any arguments and return type.
 
 import java.util.Scanner;
 
 public class MyFuncGitBranches {
+
     public static void hello(){
         System.out.println("Hello World Am Hazarudin here exploring function/methods  concepts.....");
     }
-    public static void main(String[] args) {
-        //hello();
-
+     //function with no arguments and no return type.
+    public static void calculateSum(){
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a value : ");
         int a = sc.nextInt();
@@ -19,6 +23,9 @@ public class MyFuncGitBranches {
         int sum = a + b;
         System.out.println("Sum of "+a+" and "+b+" is : "+sum);
         sc.close();
-
+    }
+    public static void main(String[] args) {
+        //hello();
+        calculateSum(); //function calling
     }
 }
