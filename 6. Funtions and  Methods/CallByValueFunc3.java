2f69ceafd4299a61bafb075823282ced86b70f98
MohemmadShaik <mohemmadshaik@gmail.com>
2024-09-04 02:14:44 +0530

    Started 11.6.1 Call by value in java as  Program1 - main branch

--- 6. Funtions and  Methods/CallByValueFunc3.java
+++ 6. Funtions and  Methods/CallByValueFunc3.java
@@ -1,12 +1,13 @@
 //11.6 Call by Value in Java
 
+//11.6.1 
 public class CallByValueFunc3{
-    public static void modifyPrimitiveType(int num){
+    public static void modifyInt(int num){
         num = 15;
     }
     public static void main(String[] args) {
         int a = 5;
-        modifyPrimitiveType(a); //Call by value
-        System.out.println("After modifyPrimitiveType( ) : "+a); // output: 5
+        modifyInt(a); //Call by value since int is a primitive type
+        System.out.println("After modifyInt( ) : "+a); // output: 5
     }
 }
