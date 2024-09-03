a1a49b82ae5e4d6037f1e570561a20419878164f
MohemmadShaik <mohemmadshaik@gmail.com>
2024-09-04 01:52:21 +0530

    11.6.2 Added pass by reference value in 11.6.1

--- 6. Funtions and  Methods/CallByValueFunc3.java
+++ 6. Funtions and  Methods/CallByValueFunc3.java
@@ -2,11 +2,18 @@
 
 public class CallByValueFunc3{
     public static void modifyPrimitiveType(int num){
-        num = 15;
+        num = 30; // This change will not affect the original value
+    }
+    public static void modifyArray(int[] arr){
+        arr[0]=25; // This change will affect the original array
     }
     public static void main(String[] args) {
-        int a = 5;
+        int a = 10;
         modifyPrimitiveType(a); //Call by value
-        System.out.println("After modifyPrimitiveType( ) : "+a); // output: 5
+        System.out.println("After modifyPrimitiveType( ) : "+a); // output: 10
+
+        int[] num = {1,2,3};
+        modifyArray(num); // Pass by reference value
+        System.out.println("After modifyArray( ) : "+num[0]); // output: 25
     }
 }
