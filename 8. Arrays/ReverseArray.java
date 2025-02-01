4dc62f1e84f6ed8758fd38f49e30926441114fa3
MohemmadShaik <mohemmadshaik@gmail.com>
2025-02-01 19:24:50 +0530

    8.Binary search Program 1 : Finding the index of an element in an array and Program 2: reversing the given array completed and also added pics of Binary search .

---
+++ 8. Arrays/ReverseArray.java
@@ -0,0 +1,21 @@
+//reverse the elements in an array
+public class ReverseArray {
+    public static void reverse(int arr[]) {
+        int start = 0, end = arr.length - 1;
+        while (start < end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 20, 30, 49, 67, 78, 88, 90 };
+        reverse(arr);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
