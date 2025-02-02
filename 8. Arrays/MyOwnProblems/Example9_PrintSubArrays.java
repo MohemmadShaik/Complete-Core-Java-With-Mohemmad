01436a46ad12dc2120f3761686b37dbf826a6661
MohemmadShaik <mohemmadshaik@gmail.com>
2025-02-02 21:38:00 +0530

    Example9 problem part 1 completed successfully.

---
+++ 8. Arrays/MyOwnProblems/Example9_PrintSubArrays.java
@@ -0,0 +1,29 @@
+//Example 9 : To print all the sub arrays of an array arr[] ={2,4,6,8,10} and also print the total number of sub arrays and also total number of pairs.
+package MyOwnProblems;
+
+public class Example9_PrintSubArrays {
+	public static void subArrayPrint(int arr[]){
+        int totalSubArray = 0;
+        System.out.println();
+		//start
+		for(int i=0; i<arr.length; i++){
+			//end
+			for(int j=i; j<arr.length; j++){
+				//print subarray
+				for(int k=i; k<=j; k++){
+					System.out.print(arr[k]+" ");
+				}
+                totalSubArray++;
+                System.out.println();
+			}
+            System.out.println();
+            System.out.println("* * * * * * * * ");
+		}
+        System.out.println("-----------------------------");
+        System.out.println("Total number of pairs : "+totalSubArray+"\n");
+	}
+    public static void main(String[] args){
+		int arr[] = {2,4,6,8,10};
+		subArrayPrint(arr);
+	}
+}
--- 8. Arrays/Practice8_8_SubArray.java
+++ 8. Arrays/Practice8_8_SubArray.java
@@ -1,4 +1,4 @@
-// Program: To print all the sub arrays of an array arr[] ={2,4,6,8,10} and also print the total number of sub arrays.
+// Practice 9: To print all the sub arrays of an array arr[] ={2,4,6,8,10} and also print the total number of sub arrays.
 public class Practice8_8_SubArray {
     public static void subArray(int arr[]) {
         for (int i = 0; i < arr.length; i++) {
