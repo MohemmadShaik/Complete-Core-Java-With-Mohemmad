25912485d867c38d3ea0f275000c0c10d4679023
MohemmadShaik <mohemmadshaik@gmail.com>
2025-02-02 12:40:18 +0530

    Example1 problem completed successfully.

--- 8. Arrays/MyOwnProblems/Example1.java
+++ 8. Arrays/MyOwnProblems/Example1.java
@@ -1,5 +1,18 @@
 package MyOwnProblems;
 
-public class Example1 {
-    
+public class Example1{
+	public static void arrayIncrement(int arr[]){
+		for(int i=0; i<arr.length; i++){
+			arr[i] = arr[i]+10;
+		}	
+	}
+	public static void main(String[] args){
+		int arr[] = {2,4,6,8,10,12};
+		arrayIncrement(arr);
+		
+        for(int i =0 ; i<arr.length; i++){
+			System.out.print(arr[i]+" ");
+		}
+		System.out.println();
+	}
 }
