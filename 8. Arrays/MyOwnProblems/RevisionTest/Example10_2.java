97b7fd8054a36556a8a7bfa44023a94392ba8367
MohemmadShaik <mohemmadshaik@gmail.com>
2025-02-15 20:13:47 +0530

    Example 10_2 Rivision PART-A, PART-B Complted.

--- 8. Arrays/MyOwnProblems/RevisionTest/Example10_2.java
+++ 8. Arrays/MyOwnProblems/RevisionTest/Example10_2.java
@@ -1,7 +1,7 @@
 /*Example10 PART A , PART B , PART C and PART D are individually dicussed in the code snippet.*/
 
 package MyOwnProblems.RevisionTest;
-
+/* 
 //PART-A : To print the sub arrays of an arr[] = {10,20,30,40,50}.
 class Example10_2{
 	public static void example10_2_Method(int myarr[]){
@@ -26,6 +26,43 @@ class Example10_2{
 		example10_2_Method(myarr);
 	}
 }
+
+*/
+
+
+//PART-B : Now print the no.of sub arrays count of an arr[] ={10,20,30,40,50}.
+
+class Example10_2{
+	public static void example10_2_Method(int myarr[]){
+        int count_All_subArray = 0;
+		//start
+		for(int i=0; i<myarr.length; i++){
+			//end
+			for(int j=i; j<myarr.length; j++){
+				//print subarray
+				for(int k=i; k<=j; k++){
+					System.out.print(myarr[k]+" ");
+				}
+                count_All_subArray++;
+                System.out.println();
+                
+			}
+            System.out.println("- - - - - - - -\n");
+		}
+        System.out.println("\n---------------------------------------------\n");
+        System.out.println("Total number of sub arrays count is : "+count_All_subArray);
+        System.out.println("\n---------------------------------------------\n");
+	}
+	public static void main(String[] args){
+		int myarr[] ={10,20,30,40,50};
+        System.out.println("\nPART-A : To print the sub arrays of an arr[] ={10,20,30,40,50}.\n");
+		example10_2_Method(myarr);
+	}
+}
+
+
+
+
 
 
 
