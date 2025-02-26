ffa17f7cf426764e9dd49e29be2ecaf74b8bd1f0
MohemmadShaik <mohemmadshaik@gmail.com>
2025-02-26 20:17:07 +0530

    13.17 Best time to Buy and Sell Stocks updated with comments completed.

--- 8. Arrays/Practice8_10_StockMarket.java
+++ 8. Arrays/Practice8_10_StockMarket.java
@@ -2,15 +2,17 @@
 
 public class Practice8_10_StockMarket{
 	public static int bestStock(int prices[]){
-		int buyingPrice = Integer.MAX_VALUE;
+		int buyingPrice = Integer.MAX_VALUE; //initially buying price is max value to track the lowest(minimum) buying price
 		int maxProfit = 0;
 		//Key note : selling price = prices[];
 		for(int i=0; i<prices.length; i++){
+			//case 1 : if buying price is less than the selling price then calculate the profit
 			if(buyingPrice<prices[i]){
 				int profit = prices[i] - buyingPrice;
 				maxProfit = Math.max(maxProfit, profit);
 			
-			}else{
+			}//case 2 : instead of selling the stock for loss, buy the stock for the future means update the buying price with the current selling price.
+			else{
 				buyingPrice = prices[i];
 			}
 		}
